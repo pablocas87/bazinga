b429815a8fb2558fc7895fd8538c20f0673698af
Pablo Castiglioni Amorós <pablocas.87@gmail.com>
2024-12-04 18:08:00 -0300

    lang es correction

--- admin/crear_usuario.php
+++ admin/crear_usuario.php
@@ -54,7 +54,7 @@ $conn->close();
 ?>
 
 <!DOCTYPE html>
-<html lang="en">
+<html lang="es">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
--- admin/ver_reservas.php
+++ admin/ver_reservas.php
@@ -22,7 +22,7 @@ $result = $conn->query($sql);
 ?>
 
 <!DOCTYPE html>
-<html lang="en">
+<html lang="es">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
